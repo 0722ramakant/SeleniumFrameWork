11df94a9f20eae14850b65caca1f98fcba971d60
ramakant shakya <shakyaramakant41@gmail.com>
2022-04-11 23:12:07 +0530

    updated First

---
+++ target/classes/base.java
@@ -0,0 +1,79 @@
+package resources;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class base {
+
+	public  WebDriver driver;
+	public Properties prop;
+public WebDriver initializeDriver() throws IOException
+{
+	
+ prop= new Properties();
+FileInputStream fis=new FileInputStream("C:\\Users\\Ramakant\\eclipse-workspace\\first\\src\\main\\java\\resources\\data.properties");
+
+prop.load(fis);
+String browserName=prop.getProperty("browser");
+System.out.println(browserName);
+
+if(browserName.equals("chrome"))
+{
+	 System.setProperty("webdriver.chrome.driver", "H:\\chromedriver_win32\\chromedriver.exe");
+	driver= new ChromeDriver();
+		//execute in chrome driver
+	
+}
+else if (browserName.equals("firefox"))
+{
+	 driver= new FirefoxDriver();
+	//firefox code
+}
+
+else if (browserName.equals("Remotechrome"))
+{
+	DesiredCapabilities caps=new DesiredCapabilities();
+	caps.setBrowserName("chrome");
+	
+	
+    driver=new RemoteWebDriver(new URL("http://192.168.112.164:4444"),caps);//hub url
+	driver.manage().window().maximize();
+	driver.manage().deleteAllCookies();
+
+}
+else if (browserName.equals("IE"))
+{
+//	IE code
+}
+
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+return driver;
+
+
+}
+
+
+public String getScreenShotPath(String testCaseName,WebDriver driver) throws IOException
+{
+	TakesScreenshot ts=(TakesScreenshot) driver;
+	File source =ts.getScreenshotAs(OutputType.FILE);
+	String destinationFile = System.getProperty("user.dir")+"\\reports\\"+testCaseName+".png";
+	FileUtils.copyFile(source,new File(destinationFile));
+	return destinationFile;
+
+
+}
+
+}
